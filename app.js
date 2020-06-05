a65aea4441456c3e3472dafad4f2d71c38c9c0c8
psh56332 <psh56332@naver.com>
2020-06-05 17:29:02 +0900

    app.js 주석을 달았습니다.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//주석 처리합니다. 
 var app = express();
 
 // view engine setup
